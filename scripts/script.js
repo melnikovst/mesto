b947ad1f3f799b00b7d8d29608b078e1cd40588d
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-05-14 22:14:33 +0300

    done

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,30 @@
-const popupOpened = document.querySelector('.profile__title-button');
-const popup = document.querySelector('.popup');
-const popupClosed = document.querySelector('.popup__button-escape');
-
+let popupOpened = document.querySelector('.profile__title-button');
+let popup = document.querySelector('.popup');
+let popupClosed = document.querySelector('.popup__button-escape');
+let formElement = document.querySelector('.popup__form-container');
+let nameInput = document.querySelector('.form__string-name');
+let jobInput = document.querySelector('.form__string-job');
+let job = document.querySelector('.profile__subtitle');
+let author = document.querySelector('.profile__title');
 
 function open () {
     popup.classList.add('popup__opened');
+    nameInput.value = author.textContent;
+    jobInput.value = job.textContent; 
 };
 
 function close () {
     popup.classList.remove('popup__opened');
 };
 
-popupClosed.addEventListener('click', close);
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    author.textContent = nameInput.value;
+    job.textContent = jobInput.value;
+    close();
+}
+
 popupOpened.addEventListener('click', open);
+popupClosed.addEventListener('click', close);
+formElement.addEventListener('submit', formSubmitHandler); 
+
