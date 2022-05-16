ad2c09b477978a96fb0e6355361c6f63fe656246
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-05-16 12:05:21 +0300

    some mistakes

--- scripts/script.js
+++ scripts/script.js
@@ -8,13 +8,13 @@ let job = document.querySelector('.profile__subtitle');
 let author = document.querySelector('.profile__title');
 
 function open () {
-    popup.classList.add('popup__opened');
+    popup.classList.add('popup_opened');
     nameInput.value = author.textContent;
     jobInput.value = job.textContent; 
 };
 
 function close () {
-    popup.classList.remove('popup__opened');
+    popup.classList.remove('popup_opened');
 };
 
 function formSubmitHandler (evt) {
